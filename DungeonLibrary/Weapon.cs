435ff10e4ec73ea52b2628067e5550d191ec83d3
Holly Ayers <ayers.hc@outlook.com>
2023-02-21 14:25:17 -0600

    wireframed weapon and character classes

---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //Make it public!
+    public class Weapon
+    {
+        //Fields
+        /*
+         * int for min damage (cannot be less than 0, or > max dmg)
+         * int for max damage (cannot be less than 0) -- assign first in the ctor
+         * string for the name of the weapon
+         * int for bonusHitChance
+         * bool isTwoHanded
+         */
+
+        //Properties - 1 for each field
+
+        //CTORS/Constructors - 1 qualified; and one unqualified (default) if you want Object Initialization Syntax
+
+        //Methods
+        //Nicely formatted ToString() override
+    }
+}
