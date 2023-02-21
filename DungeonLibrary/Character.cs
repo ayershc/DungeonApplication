badd2b723e9ae487ebd5b39b591df68e8d7414e9
Holly Ayers <ayers.hc@outlook.com>
2023-02-21 15:50:07 -0600

    DungeonApp Custom Class Framework

--- DungeonLibrary/Character.cs
+++ DungeonLibrary/Character.cs
@@ -3,7 +3,11 @@
     public class Character
     {
         //Fields
-
+        private string _name;
+        private int _maxLife;
+        private int _life;
+        private int _hitChance;
+        private int _block;
         /*
          * int life (cannot be more than maxlife
          * int maxlife (assign maxlife before life in ctor or life will error)
@@ -13,14 +17,69 @@
          */
 
         //Properties - 1 for each field
+        
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int MaxLife
+        {
+            get { return _maxLife; }
+            set { _maxLife = value; }
+        }
+        public int Life
+        {
+            get { return _life; }
+            set { _life = value; }
+        }
+        public int HitChance
+        {
+            get { return _hitChance; }
+            set { _hitChance = value; }
+        }
+        public int Block
+        {
+            get { return _block; }
+            set { _block = value; }
+        }
 
         //CTORS/Constructors - 1 fully qualified, 1 default/unqualified
+        public Character() { }//default
+
+        public Character(string name, int maxLife, int life, int hitChance, int block)
+        {
+            Name = name;
+            MaxLife = maxLife;
+            Life = life;
+            HitChance = hitChance;
+            Block = block;
+        }
 
         //Methods
         //ToString() override Name, Life/MaxLife, Hit chance, Block
-
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"{Name}\n" +
+                $"HP: {Life}/{MaxLife}\n" +
+                $"Hit Chance: {HitChance}\n" +
+                $"Block: {Block}";
+        }
         //CalcBlock() returns an int -> return Block;
+        public int CalcBlock()
+        {
+            return Block;
+        }
         //CalcHitChance() returns an int -> return HitChance;
+        public int CalcHitChance()
+        {
+            return HitChance;
+        }
         //CalcDamage() returns an int -> return 0;
+        public int CalcDamage()
+        {
+            return 0;
+        }
     }
 }
--- DungeonLibrary/Weapon.cs
+++ DungeonLibrary/Weapon.cs
@@ -17,12 +17,60 @@ namespace DungeonLibrary
          * int for bonusHitChance
          * bool isTwoHanded
          */
+        private string _weaponName;
+        private int _minDmg;
+        private int _maxDmg;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
 
         //Properties - 1 for each field
+        public string WeaponName
+        {
+            get { return _weaponName; }
+            set { _weaponName = value; }
+        }
+        public int MinDmg
+        {
+            get { return _minDmg; }
+            set { _minDmg = value; }
+        }
+        public int MaxDmg
+        {
+            get { return _maxDmg; }
+            set { _maxDmg = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set { _bonusHitChance = value; }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
 
         //CTORS/Constructors - 1 qualified; and one unqualified (default) if you want Object Initialization Syntax
+        public Weapon() { }//default
+
+        public Weapon(string weaponName, int minDmg, int maxDmg, int bonusHitChance, bool isTwoHanded)
+        {
+            WeaponName = weaponName;
+            MinDmg = minDmg;
+            MaxDmg = maxDmg;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;          
+        }
 
         //Methods
         //Nicely formatted ToString() override
+        public override string ToString()
+        {
+            //return base.ToString();
+            return $"Weapon: {WeaponName}\n" +
+                $"{(IsTwoHanded ? "Two-" : "One-")}Handed\n" +
+                $"Damage: {MinDmg} - {MaxDmg}\n" +
+                $"Crit Chance: {BonusHitChance}\n";
+        }
     }
 }
